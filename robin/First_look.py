9dd2cebede08a3f423bee5bb83d521003a400d4e
Robin  van der Veken <robinvanderveken@Robins-MacBook-Pro.local>
2020-04-17 14:48:12 +0200

    first running moddel :O

--- robin/First_look.py
+++ robin/First_look.py
@@ -1,10 +1,36 @@
 # import
 from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
 
 # data
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-train_images.shape
-len(train_labels)
-train_labels
 
-test_images.shape
+# network architecture
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+# compile network
+network.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+# prep images
+train_images = train_images.reshape((60000, 28*28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28*28))
+test_images = test_images.astype('float32') / 255
+
+# prep labels
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# train network
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# check results
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc: ', test_acc)
